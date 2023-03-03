7d7cd7b4718049a310756353f5ec084f9543019d
bae0203 <gmbae06@dgsw.hs.kr>
2023-03-03 14:15:43 +0900

    ADD :: Todo All Delete

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,5 @@
 import Todo from "./components/Todo";
+import Location from "./components/Location";
 import { useEffect, useState, KeyboardEvent } from "react";
 import * as S from "./style/AppStyled";
 
@@ -21,11 +22,15 @@ function App() {
         textTodo + "를 할 일에 추가하시겠습니까?"
       );
       if (TodoADDCheck) {
-        let todoValueADD: string[] = [...todoContext, textTodo];
-        setTodoContext([...todoValueADD]);
-        const JSONConversion: string = JSON.stringify(todoValueADD);
-        localStorage.setItem("todoContext", JSONConversion);
-        setTextTodo("");
+        if (textTodo.length > 25) {
+          alert("내용이 너무 깁니다.");
+        } else {
+          let todoValueADD: string[] = [...todoContext, textTodo];
+          setTodoContext([...todoValueADD]);
+          const JSONConversion: string = JSON.stringify(todoValueADD);
+          localStorage.setItem("todoContext", JSONConversion);
+          setTextTodo("");
+        }
       }
     }
   };
@@ -35,29 +40,45 @@ function App() {
     }
   };
 
+  const AllDeleteButton = () => {
+    let CheckDelete = window.confirm("정말 다 삭제하실건가요?");
+    console.log(CheckDelete);
+    if (CheckDelete) {
+      setTodoContext([]);
+      localStorage.clear();
+    }
+  };
+
   return (
     <>
-      <S.Title>Todo List</S.Title>
-      <S.TodoInput
-        type={"text"}
-        placeholder={"추가할 할 일을 입력해주세요"}
-        value={textTodo}
-        onChange={(e) => {
-          setTextTodo(e.target.value);
-        }}
-        onKeyDown={Enterhandler}
-      />
-      <S.TodoButton onClick={ADDTodohandler}>추가</S.TodoButton>
-      {todoContext.map((value: string, index: number) => {
-        return (
-          <Todo
-            context={value}
-            index={index}
-            entireTodoContext={todoContext}
-            setEntireTodoContext={setTodoContext}
-          ></Todo>
-        );
-      })}
+      <S.TodoArea>
+        <S.Title>Todo List</S.Title>
+        <S.TodoInput
+          type={"text"}
+          placeholder={"추가할 할 일을 입력해주세요"}
+          value={textTodo}
+          onChange={(e) => {
+            setTextTodo(e.target.value);
+          }}
+          onKeyDown={Enterhandler}
+        />
+        <S.TodoButton onClick={ADDTodohandler}>추가</S.TodoButton>
+        <S.TodoContentArea>
+          {todoContext.map((value: string, index: number) => {
+            return (
+              <Todo
+                context={value}
+                index={index}
+                entireTodoContext={todoContext}
+                setEntireTodoContext={setTodoContext}
+              ></Todo>
+            );
+          })}
+        </S.TodoContentArea>
+        <S.TodoButton onClick={AllDeleteButton}>전체 삭제</S.TodoButton>
+      </S.TodoArea>
+
+      <Location></Location>
     </>
   );
 }
--- src/components/Todo.tsx
+++ src/components/Todo.tsx
@@ -11,35 +11,33 @@ interface ITodoContent {
 const Todo = (props: ITodoContent) => {
   const [check, setCheck] = useState<boolean>(false);
   return (
-    <>
-      <Tododiv>
-        <input
-          type="checkbox"
-          checked={check}
-          onChange={() => {
-            setCheck(!check);
-          }}
-        />
-        <TodoContent isCheck={check}>{props.context}</TodoContent>
-        <TodoDel
-          isDone={check}
-          onClick={() => {
-            let DeleteCheck: boolean = window.confirm(
-              "'" + props.context + "'을(를) 삭제하실건가요?"
-            );
-            if (DeleteCheck) {
-              props.entireTodoContext.splice(props.index, 1);
-              let array: string[] = [...props.entireTodoContext];
-              props.setEntireTodoContext([...array]);
-              const JSONConversion: string = JSON.stringify(array);
-              localStorage.setItem("todoContext", JSONConversion);
-            }
-          }}
-        >
-          ✕
-        </TodoDel>
-      </Tododiv>
-    </>
+    <Tododiv>
+      <input
+        type="checkbox"
+        checked={check}
+        onChange={() => {
+          setCheck(!check);
+        }}
+      />
+      <TodoContent isCheck={check}>{props.context}</TodoContent>
+      <TodoDel
+        isDone={check}
+        onClick={() => {
+          let DeleteCheck: boolean = window.confirm(
+            "'" + props.context + "'을(를) 삭제하실건가요?"
+          );
+          if (DeleteCheck) {
+            props.entireTodoContext.splice(props.index, 1);
+            let array: string[] = [...props.entireTodoContext];
+            props.setEntireTodoContext([...array]);
+            const JSONConversion: string = JSON.stringify(array);
+            localStorage.setItem("todoContext", JSONConversion);
+          }
+        }}
+      >
+        ✕
+      </TodoDel>
+    </Tododiv>
   );
 };
 
