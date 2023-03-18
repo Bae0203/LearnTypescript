a6f13c4f0da0dd77ffdc25b18b84695ad062ac04
bae0203 <gmbae06@dgsw.hs.kr>
2023-03-18 17:16:48 +0900

    FIX :: File Input Styled

--- src/style/TestProjStyle.tsx
+++ src/style/TestProjStyle.tsx
@@ -1,4 +1,5 @@
 import styled from "styled-components";
+import FileImage from "../style/asset/file.svg";
 
 export const InputWrap = styled.nav`
   margin-bottom: 1rem;
@@ -33,6 +34,40 @@ export const InputStyle = styled.input`
   :-ms-input-placeholder {
     color: #c0c0c0;
   }
+  &:hover {
+    border: 1px solid #a9c0f6;
+  }
+  &:active{
+    border 1px solid #1556F7;
+  }
+`;
+
+export const FileInputStyle = styled.input`
+  position: absolute;
+  opacity: 0;
+`;
+
+export const FileInputLabel = styled.label`
+  display: flex;
+  padding: 0.8rem 0.5rem;
+  font-size: 0.8rem;
+  width: 22.125rem;
+  box-shadow: none;
+  border: 1px solid #c0c0c0;
+  border-radius: 5px;
+  color: #c0c0c0;
+  &:hover {
+    border: 1px solid #a9c0f6;
+  }
+  &:active{
+    border 1px solid #1556F7;
+  }
+`;
+
+export const FileImageStyle = styled.img`
+  width: 1rem;
+  height: 1rem;
+  margin: 0rem 0.5rem;
 `;
 
 export const ButtonStyle = styled.button`
